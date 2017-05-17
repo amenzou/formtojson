01475a71373d3e00e808ebb600e2222e32a39f34
Amenzou Ouidja <amenzou.ouidja@gmail.com>
2017-05-17 00:28:55 -0400

    Add strict mode
    Use binary for checkboxes
    Cast select[multiple] to array

--- jquery.formtojson.js
+++ jquery.formtojson.js
@@ -1,26 +1,38 @@
 (function($) {
-  $.fn.formToJson = function() {
+  $.fn.formToJson = function(options) {
+ 		 var settings = $.extend({
+    	strict: true
+    }, options);
   	var params = {};
     var rawParams = this.serializeArray();
-    this.find('input[type="checkbox"]:not(:checked)').each(function(){
-      rawParams.push({name: this.name, value: 0});
-    });
-    this.find('select[multiple]').each(function(){
-      if(!$(this).find('option:selected').length){
-        rawParams.push({name: this.name, value: []});            
-      }
-    });
+    console.log(rawParams);
     $.each(rawParams, function(i, keyValue) {
       //if it's a multi value field (e.g.: <select multiple>)
       if (params.hasOwnProperty(keyValue.name)) {
         params[keyValue.name] = $.makeArray(params[keyValue.name]);
         params[keyValue.name].push(keyValue.value);
-      }
-      else {
-        //If it's a checkbox use 1 instead of 'on'
-        params[keyValue.name] = (keyValue.value == 'on') ? 1 : keyValue.value;
+      } else {
+      	params[this.name] = this.value;
       }
     });
+    if (settings.strict) {
+      // Use binary for checkboxes
+    	this.find('input[type="checkbox"]').each(function(){
+        params[this.name] = 0;
+        if ($(this).not(':checked')) {
+        	params[this.name] = 1;
+        }
+      });
+      // Cast select[multiple] to array 
+      this.find('select[multiple]').each(function(){
+      	var count = $(this).find('option:selected').length;
+        if(count == 0 ){
+          params[this.name] = [];            
+        } else if (count == 1) {
+        	params[this.name] = [this.value]; 
+        }
+      });
+    }
     return params;
   };
-}(jQuery))
+}(jQuery));
