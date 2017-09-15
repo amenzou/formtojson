b7bcbd4e1d3ee79c7b8b762c1adcf15dbd8b083d
Amenzou Ouidja <amenzou@ouidja.com>
2017-09-14 23:28:08 -0400

    Rename option 'strict' to 'cast'

--- jquery.formtojson.js
+++ jquery.formtojson.js
@@ -1,11 +1,10 @@
 (function($) {
   $.fn.formToJson = function(options) {
- 		 var settings = $.extend({
-    	strict: true
+ 		var settings = $.extend({
+    	cast: true
     }, options);
   	var params = {};
     var rawParams = this.serializeArray();
-    console.log(rawParams);
     $.each(rawParams, function(i, keyValue) {
       //if it's a multi value field (e.g.: <select multiple>)
       if (params.hasOwnProperty(keyValue.name)) {
@@ -15,18 +14,18 @@
       	params[this.name] = this.value;
       }
     });
-    if (settings.strict) {
-      // Use binary for checkboxes
+    if (settings.cast) {
+      // Use tinyint for checkboxes
     	this.find('input[type="checkbox"]').each(function(){
         params[this.name] = 0;
-        if ($(this).not(':checked')) {
+        if (this.is(':checked')) {
         	params[this.name] = 1;
         }
       });
       // Cast select[multiple] to array 
       this.find('select[multiple]').each(function(){
-      	var count = $(this).find('option:selected').length;
-        if(count == 0 ){
+      	var count = this.find('option:selected').length;
+        if(count === 0 ){
           params[this.name] = [];            
         } else if (count == 1) {
         	params[this.name] = [this.value]; 
